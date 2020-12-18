2d83722d712e6d6e31686d7db2b2472786ba35de
Fernando Balaguer <ferbm00@gmail.com>
2020-12-18 19:10:42 +0100

    algo del examen php

--- DWS/Examen1eval/fernando.php
+++ DWS/Examen1eval/fernando.php
@@ -17,6 +17,72 @@ class Bd
 		return $this->$var;
 	}
 }
-class Conexion{
-    
+class Conexion
+{
+	private $conexion;
+
+	static function getAll($link)
+	{
+		$consulta = "SELECT * FROM clientes";
+		return $result = $link->query($consulta);
+	}
+	function __construct($conexion)
+	{
+		$this->conexion = $conexion;
+	}
+}
+class Alquileres
+{
+	private $idAlquiler;
+	private $pelicula;
+	private $cliente;
+	private $empleado;
+
+	function __construct($idAlquiler, $pelicula, $cliente, $empleado)
+	{
+		$this->idAlquiler = $idAlquiler;
+		$this->pelicula = $pelicula;
+		$this->cliete = $cliente;
+		$this->empleado = $empleado;
+	}
+	function getIdAlquiler()
+	{
+		return $this->idAlquiler;
+	}
+	function setIdAlquiler($idAlquiler)
+	{
+		$this->idAlquiler = $idAlquiler;
+	}
+	function getPelicula()
+	{
+		return $this->pelicula;
+	}
+	function setPelicula($pelicula)
+	{
+		$this->pelicula = $pelicula;
+	}
+	function getCliente()
+	{
+		return $this->cliente;
+	}
+	function setCliente($cliente)
+	{
+		$this->cliente = $cliente;
+	}
+	function getEmpleado()
+	{
+		return $this->empleado;
+	}
+	function setEmpleado($empleado)
+	{
+		$this->empleado = $empleado;
+	}
+	function existe()
+	{
+	}
+	function instertar()
+	{
+		$consulta = "INSERT INTO alquileres VALUES ('$this->idAlquiler', CURDATE(), '$this->pelicula','$this->cliente','$this->empleado')";
+		return $link->query($consulta);
+	}
 }
