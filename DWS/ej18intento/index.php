04744e7bdaa18042220fb9e6166b6a12638a2173
Fernando Balaguer <ferbm00@gmail.com>
2021-01-15 19:14:52 +0100

    php cookies agh

---
+++ DWS/ej18intento/index.php
@@ -0,0 +1,18 @@
+<?php
+setcookie('nombre','',-360000);
+require "modelo.php";
+$base= new Base();
+if (isset($_POST['enviar'])) {
+	$ped= new Pedido ($_POST['idPedido'],$_POST['fecha'],$_POST['Cliente']);
+	if (!$ped->existe($base->link)) {
+		$_SESSION['linea']=0;
+		$ped->guardar();
+		header('Location:lineas.php');
+	} else {
+		$dato="Error: ya existe este pedido <br/>";
+		require "vistas/mensaje.php";
+	}
+}else{
+	require "funcion.php";
+	require "vistas/pedido.php";
+}
