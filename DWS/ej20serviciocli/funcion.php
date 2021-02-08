06cbf4c1f96ae96a2b49f2834fd6c9a5aec16302
Fernando Balaguer <ferbm00@gmail.com>
2021-02-08 21:13:07 +0100

    proyecto 2ev XD na

--- DWS/ej20serviciocli/funcion.php
+++ DWS/ej20serviciocli/funcion.php
@@ -4,7 +4,7 @@ function lista ($link,$tabla, $nomid, $mostrar){
     $consulta=json_decode(file_get_contents($url), true);
     $string= "<select name='$tabla'>";
     foreach($consulta as $key => $fila){
-       $string.= "<option value='".$fila[$nomid]."'>".$fila[$mostrar]."</option>";
+       $string.= "<option value='".$fila[$nomid]."'>".$fila[$mostrar]."</option>";''
     }
     $string.= "</select>";
   
